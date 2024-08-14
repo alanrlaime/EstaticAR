a38fd9f24c32c51add3eb985426a013d05b073a5
alanrlaime <alanrodrigolaime@gmail.com>
2024-08-13 21:24:06 -0400

    Comentarios

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+//Para guardar en local: git add .
+//Para comentar: git commit -m "Mensaje"
+//Para subir a red: git push origin main
+//Para actualizar repo local: git pull
+
+
 alert("Bienvenidos :D")
